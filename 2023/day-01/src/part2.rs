dcbe96f53c96359a04e0131e4f59b9a9a4566970
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-12-06 22:28:58 +0000

    minor improvements

--- 2023/day-01/src/part2.rs
+++ 2023/day-01/src/part2.rs
@@ -1,28 +1,28 @@
 use crate::prelude::*;
 
 #[tracing::instrument(skip_all)]
-pub fn process(input: &str) -> miette::Result<u32, AocError> {
+pub fn process(input: &str) -> miette::Result<usize, AocError> {
     Ok(input
         .lines()
         .map(process_line) //for each line, plug it into `proccess_line`
-        .sum::<u32>())
+        .sum::<usize>())
 }
 
 /// Returns the
 #[tracing::instrument]
-fn process_line(line: &str) -> u32 {
+fn process_line(line: &str) -> usize {
     /// all possible numbers in their word forms.
     const NUMBERWORDS: [&str; 9] = [
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
 
     let line_len = line.len();
-    let nums: Vec<u32> = (0..line_len) //iterate for the length of `line`
+    let nums: Vec<usize> = (0..line_len) //iterate for the length of `line`
         .filter_map(|index| {
             let ch = line.as_bytes()[index]; //the character at the index `index` of this line.
             if ch.is_ascii_digit() {
                 return Some(
-                    (ch as u32) - 48, // -48 for the ascii offset
+                    (ch as usize) - 48, // -48 for the ascii offset
                 );
             }
 
@@ -30,13 +30,14 @@ fn process_line(line: &str) -> u32 {
                 //check if a line slice beginning from the index starts with one of the numbers in word form
                 if (line_len - index) >= 3 && line[index..].starts_with(numberword) {
                     Some(
-                        (i + 1) as u32, // +1 as NUMBERWORDS starts with index 0, but the first term is "one"
+                        i + 1, // +1 as NUMBERWORDS starts with index 0, but the first term is "one"
                     )
                 } else {
                     None // as this is a `filter_map()`, returning `None` will remove this value from the iterator.
                 }
             })
-        });
+        })
+        .collect();
 
     let first = nums.first().expect("there to be at least 1 number");
     let last = nums.last().expect("there to be at least 1 number");
@@ -63,7 +64,7 @@ mod tests {
     /// it tests two overlapping numbers
     /// where the second number should succeed
     #[case("fivezg8jmf6hrxnhgxxttwoneg", 51)]
-    fn line_test(#[case] line: &str, #[case] expected: u32) {
+    fn line_test(#[case] line: &str, #[case] expected: usize) {
         assert_eq!(expected, process_line(line))
     }
 
