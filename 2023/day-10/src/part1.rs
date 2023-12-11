ffdc4f26cf89b6587a76c3b9021aff9670ce4846
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-12-11 20:01:17 +0000

    remove some tracing + benchmarks.txt

--- 2023/day-10/src/part1.rs
+++ 2023/day-10/src/part1.rs
@@ -2,10 +2,6 @@ use std::collections::HashMap;
 
 use glam::IVec2;
 
-use tracing::info;
-
-// use nom::branch::
-
 use crate::custom_error::AocError;
 
 #[tracing::instrument(skip(input))]
@@ -17,17 +13,11 @@ pub fn process(input: &str) -> miette::Result<u32, AocError> {
         .find_map(|(key, value)| (value == &PipeType::StartingPosition).then_some(key))
         .expect("there to be a starting position");
 
-    info!(starting_pos = ?starting_pos);
-
     let mut valid_directions = Direction::iterator()
         .filter_map(|direction| {
-            let position = *starting_pos + direction.to_vec();
+            let position = *starting_pos + direction.to_point();
             grid.get(&position)
-                .is_some_and(|pipe| {
-                    info!(pipe = ?pipe, direction = ?direction, pos = ?position);
-
-                    direction.is_pipe_connected(pipe)
-                })
+                .is_some_and(|pipe| direction.is_pipe_connected(pipe))
                 .then_some((position, *direction))
         })
         .map(|tuple| {
@@ -37,9 +27,7 @@ pub fn process(input: &str) -> miette::Result<u32, AocError> {
                     .expect("that this exists")
                     .to_direction(direction);
 
-                info!(direction = ?direction, pos = ?position);
-
-                Some((*position + next_direction.to_vec(), next_direction))
+                Some((*position + next_direction.to_point(), next_direction))
             })
         });
 
@@ -68,14 +56,11 @@ enum PipeType {
 }
 
 impl PipeType {
-    #[tracing::instrument]
-    fn to_direction(&self, from_direction: &Direction) -> Direction {
+    fn to_direction(self, from_direction: &Direction) -> Direction {
         use Direction::*;
         use PipeType::*;
 
-        info!("");
-
-        let to = match (&self, from_direction) {
+        match (self, from_direction) {
             (Vertical, South) => South,
             (Vertical, North) => North,
             (Horizontal, East) => East,
@@ -90,68 +75,11 @@ impl PipeType {
             (SouthEast, North) => East,
             _ => unreachable!(
                 "Landing on {:?} from {from_direction:?} shouldn't be possible!",
-                &self
+                self
             ),
-        };
-
-        info!(to = ?to);
-
-        to
-    }
-}
-
-#[derive(Debug, Eq, PartialEq, Clone, Copy)]
-enum Direction {
-    North,
-    East,
-    South,
-    West,
-}
-
-impl Direction {
-    fn opposite(&self) -> Direction {
-        match &self {
-            Direction::North => Direction::South,
-            Direction::East => Direction::West,
-            Direction::South => Direction::North,
-            Direction::West => Direction::East,
         }
     }
-
-    fn to_vec(&self) -> IVec2 {
-        match &self {
-            Direction::North => Point::NEG_Y,
-            Direction::East => Point::X,
-            Direction::South => Point::Y,
-            Direction::West => Point::NEG_X,
-        }
-    }
-
-    fn is_pipe_connected(&self, pipe: &PipeType) -> bool {
-        match &self {
-            Direction::North => 
-                matches!(pipe, PipeType::Vertical | PipeType::SouthEast | PipeType::SouthWest),
-            
-            Direction::East => 
-                matches!(pipe, PipeType::Horizontal | PipeType::SouthWest | PipeType::NorthWest),
-            
-            Direction::South => 
-                matches!(pipe, PipeType::Vertical | PipeType::NorthEast | PipeType::NorthWest),
-            
-            Direction::West => matches!(pipe, PipeType::Horizontal | PipeType::NorthEast | PipeType::SouthEast)
-        }
-    }
-
-    pub fn iterator() -> std::slice::Iter<'static, Direction> {
-        use Direction::*;
-        static DIRECTIONS: [Direction; 4] = [North, East, South, West];
-        DIRECTIONS.iter()
-    }
 }
-
-type Point = IVec2;
-
-type Grid = HashMap<Point, PipeType>;
 
 impl From<char> for PipeType {
     fn from(value: char) -> Self {
@@ -168,6 +96,59 @@ impl From<char> for PipeType {
     }
 }
 
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl Direction {
+    fn to_point(self) -> IVec2 {
+        match self {
+            Direction::North => Point::NEG_Y,
+            Direction::East => Point::X,
+            Direction::South => Point::Y,
+            Direction::West => Point::NEG_X,
+        }
+    }
+
+    fn is_pipe_connected(self, pipe: &PipeType) -> bool {
+        match &self {
+            Direction::North => matches!(
+                pipe,
+                PipeType::Vertical | PipeType::SouthEast | PipeType::SouthWest
+            ),
+
+            Direction::East => matches!(
+                pipe,
+                PipeType::Horizontal | PipeType::SouthWest | PipeType::NorthWest
+            ),
+
+            Direction::South => matches!(
+                pipe,
+                PipeType::Vertical | PipeType::NorthEast | PipeType::NorthWest
+            ),
+
+            Direction::West => matches!(
+                pipe,
+                PipeType::Horizontal | PipeType::NorthEast | PipeType::SouthEast
+            ),
+        }
+    }
+
+    pub fn iterator() -> std::slice::Iter<'static, Direction> {
+        use Direction::*;
+        static DIRECTIONS: [Direction; 4] = [North, East, South, West];
+        DIRECTIONS.iter()
+    }
+}
+
+type Point = IVec2;
+
+type Grid = HashMap<Point, PipeType>;
+
 fn to_grid(input: &str) -> Grid {
     input
         .lines()
@@ -181,19 +162,6 @@ fn to_grid(input: &str) -> Grid {
             })
         })
         .collect::<Grid>()
-}
-
-fn display(c: char) -> &'static str {
-    match c {
-        'S' => "S",
-        '|' => "│",
-        '-' => "─",
-        'L' => "└",
-        'J' => "┘",
-        '7' => "┐",
-        'F' => "┌",
-        _ => "·",
-    }
 }
 
 #[cfg(test)]
